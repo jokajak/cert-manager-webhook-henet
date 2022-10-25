d5ff4e98e87de4635e8f6f467d3a91cd687295c5
Diftraku <Diftraku@users.noreply.github.com>
2022-10-25 23:12:30 +0300

    Major changes to support newer versions and tests

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 	"os"
+	"strings"
 
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
-
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/klog"
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
@@ -49,9 +49,39 @@ type InternalConfig struct {
 func (c *HEnetDNSProviderSolver) Name() string {
 	return "hurricane-electric"
 }
-
 func (c *HEnetDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	klog.V(6).Infof("call function Present: namespace=%s, zone=%s, fqdn=%s", ch.ResourceNamespace, ch.ResolvedZone, ch.ResolvedFQDN)
+	err := handleRequest(c, ch, false)
+	if err != nil {
+		return err
+	}
+	klog.Infof("Succesfully presented TXT record %v", ch.ResolvedFQDN)
+	return nil
+}
+
+func (c *HEnetDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	klog.V(6).Infof("call function CleanUp: namespace=%s, zone=%s, fqdn=%s", ch.ResourceNamespace, ch.ResolvedZone, ch.ResolvedFQDN)
+	err := handleRequest(c, ch, true)
+	if err != nil {
+		return err
+	}
+	klog.Infof("Succesfully presented TXT record %v", ch.ResolvedFQDN)
+	return nil
+}
+
+func (c *HEnetDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	k8sClient, err := kubernetes.NewForConfig(kubeClientConfig)
+	klog.V(6).Infof("Input variable stopCh is %d length", len(stopCh))
+	if err != nil {
+		return err
+	}
+
+	c.client = k8sClient
+
+	return nil
+}
+
+func handleRequest(c *HEnetDNSProviderSolver, ch *v1alpha1.ChallengeRequest, cleanup bool) error {
 
 	config, err := clientConfig(c, ch)
 
@@ -65,7 +95,12 @@ func (c *HEnetDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	// The hostname should be the full hostname without a trailing dot
 	data.Set("hostname", strings.TrimRight(ch.ResolvedFQDN, "."))
 	data.Set("password", config.Password)
-	data.Set("txt", ch.Key)
+	if cleanup {
+		// "invalidate" the record to make tests happy
+		data.Set("txt", "invalidated")
+	} else {
+		data.Set("txt", ch.Key)
+	}
 
 	req, err := http.NewRequest(method, apiUrl, strings.NewReader(data.Encode()))
 	if err != nil {
@@ -102,7 +137,7 @@ func (c *HEnetDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	}
 
 	// Hail-mary check that we got a good response
-	if strings.Index(body, "good") == -1 {
+	if strings.Index(body, "good") == -1 && strings.Index(body, "nochg") == -1 {
 		text := "Error calling API, unknown response: " + body
 		klog.Error(text)
 		return errors.New(text)
@@ -111,25 +146,6 @@ func (c *HEnetDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	// At this point, we should be fine (status from server was "good")
 	text := "API responded:" + resp.Status + " url: " +  apiUrl + " method: " + method + " body: " + body
 	klog.Info(text)
-
-	klog.Infof("Succesfully presented txt record %v", ch.ResolvedFQDN)
-
-	return nil
-}
-
-func (c *HEnetDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-	// We don't handle cleanup as all dynamic records are created manually in the dns.he.net control panel
-	return nil
-}
-
-func (c *HEnetDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
-	k8sClient, err := kubernetes.NewForConfig(kubeClientConfig)
-	klog.V(6).Infof("Input variable stopCh is %d length", len(stopCh))
-	if err != nil {
-		return err
-	}
-
-	c.client = k8sClient
 
 	return nil
 }
@@ -157,9 +173,9 @@ func clientConfig(c *HEnetDNSProviderSolver, ch *v1alpha1.ChallengeRequest) (Int
 		return config, err
 	}
 	config.ApiUrl = cfg.ApiUrl
-
 	secretName := cfg.SecretRef
-	sec, err := c.client.CoreV1().Secrets(ch.ResourceNamespace).Get(secretName, metav1.GetOptions{})
+
+	sec, err := c.client.CoreV1().Secrets(ch.ResourceNamespace).Get(context.Background(), secretName, metav1.GetOptions{})
 
 	if err != nil {
 		return config, fmt.Errorf("unable to get secret `%s/%s`; %v", secretName, ch.ResourceNamespace, err)
