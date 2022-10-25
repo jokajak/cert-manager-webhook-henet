94f32edb1f91cbb23a2d7b3a005c545c712f14a0
Diftraku <Diftraku@users.noreply.github.com>
2022-10-25 23:12:30 +0300

    Fix tests

--- main_test.go
+++ main_test.go
@@ -2,42 +2,35 @@ package main
 
 import (
 	"os"
+	"time"
 	"testing"
 
 	"github.com/jetstack/cert-manager/test/acme/dns"
-
-	"github.com/cert-manager/webhook-example/example"
 )
 
 var (
 	zone = os.Getenv("TEST_ZONE_NAME")
-	fqdn string
+	pollInterval = time.Second * 10
+	propagationLimit = time.Minute * 6
 )
 
 func TestRunsSuite(t *testing.T) {
 	// The manifest path should contain a file named config.json that is a
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
+	//
 
-	fqdn = GetRandomString(20) + "." + zone
-
+	// Uncomment the below fixture when implementing your custom DNS provider
+	// Allow up-to 6 minutes for propagation, 5 minutes is the lowest TTL on HEnet
+	// Since we have such a long propagation limit, we poll every 10 seconds
 	fixture := dns.NewFixture(&HEnetDNSProviderSolver{},
 		dns.SetResolvedZone(zone),
-		dns.SetResolvedFQDN(fqdn),
 		dns.SetAllowAmbientCredentials(false),
 		dns.SetManifestPath("testdata/henet"),
-		dns.SetBinariesPath("kubebuilder/bin"),
+		dns.SetPollInterval(pollInterval),
+		dns.SetPropagationLimit(propagationLimit),
 	)
+	// You must setup all the entries manually, run only the basic set
+	fixture.RunBasic(t)
 
-	fixture.RunConformance(t)
-}
-
-func GetRandomString(n int) string {
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
 }
